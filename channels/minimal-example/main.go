37ea4432c7d0fd498272082016dd825c4c9be033
amottaz <amottaz@jumptech.com>
2018-05-18 13:42:54 -0500

    add minimal example working with channels

---
+++ channels/minimal-example/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan int)
+
+	go foo(c)
+	bar(c)
+
+	fmt.Println("Exiting main...")
+}
+
+func foo(c chan<- int) {
+	c <- 42
+}
+
+func bar(c <-chan int) {
+	fmt.Println("Bar received", <-c)
+}
